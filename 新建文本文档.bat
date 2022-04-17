853e83739912ceeb4851485f69fd611da6651abe
sdklp <90121663+sdklp@users.noreply.github.com>
2022-04-17 18:29:15 +0000

    Update 新建文本文档.bat

--- 新建文本文档.bat
+++ 新建文本文档.bat
@@ -1,15 +1,1 @@
-git config --global user.name 'sdklp'
-git config --global user.email '617100658@qq.com'  
 
-
-
-git init   
-git remote add origin https://github.com/sdklp/deviceManageSoftwareWithMongoDB.git
-
-
-
-git add *
-
-git commit -m 'first'
-
-git push -u origin master
